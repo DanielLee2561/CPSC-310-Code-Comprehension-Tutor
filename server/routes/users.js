0982d21d5b3693bffaffadb2776329e5e0539efc
Mark <1912175791@qq.com>
2024-07-04 21:16:44 -0700

    Api update

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,23 +4,31 @@ import fs from 'fs';
 import path from 'path';
 import session from 'express-session';
 
-const userJSONPath = path.join(process.cwd(), 'data', 'user.json');
+const userJSONPath = path.join(process.cwd(), 'data', 'users.json');
 
-let users;
+var users;
 
 // read user's specific question's attempts
-fs.readFile(userJSONPath, 'utf8', (err, data) => {
+fs.readFile('../data/users.json', 'utf8', (err, data) => {
     if (err) return console.error('Error reading user.json: ', err);
     try {
+        //let content = JSON.parse(data); 
         users = JSON.parse(data).users;
+        console.log(users);
     } catch (err) {
         console.error('Error parsing user.json: ', err);
     }
 });
 
+// if the data from the url, can use fetch to get the data from the frontend
+//fetch('../data/users.json')
+//    .then((response)=>response.json())
+//    .then((json)=>console.log(json));
+//console.log(users);
+
 // View Previous Attempt for a Specific Question
-router.get('/:attemptId', (req, res) => {
-    const { username, questionId, attemptId } = req.body;
+router.get('/', (req, res) => {
+    const { username, questionId, attemptId } = req.params;
     if (!username || !questionId || !attemptId)
         return res.status(400).json({error:"username, questionId and attemptId are required."});
 
