6048376bdf9fc7dfff3f66c481ff65c9d5899309
Mark <1912175791@qq.com>
2024-07-04 19:49:51 -0700

    Added api to get attempt from specific question.

---
+++ server/routes/users.js
@@ -0,0 +1,38 @@
+import express, { response } from "express";
+const router = express.Router();
+import fs from 'fs';
+import path from 'path';
+import session from 'express-session';
+
+const userJSONPath = path.join(process.cwd(), 'data', 'user.json');
+
+let users;
+
+// read user's specific question's attempts
+fs.readFile(userJSONPath, 'utf8', (err, data) => {
+    if (err) return console.error('Error reading user.json: ', err);
+    try {
+        users = JSON.parse(data).users;
+    } catch (err) {
+        console.error('Error parsing user.json: ', err);
+    }
+});
+
+// View Previous Attempt for a Specific Question
+router.get('/:attemptId', (req, res) => {
+    const { username, questionId, attemptId } = req.body;
+    if (!username || !questionId || !attemptId)
+        return res.status(400).json({error:"username, questionId and attemptId are required."});
+
+    const user = users.find(c => c.username === username);
+    if (!user) return res.status(404).send('User is not found.');
+
+    const question = user.questions.find(c => c.questionId === questionId);
+    if (!question) return res.status(404).send('Question is not found.');
+
+    const attempt = question.attempts[attemptId-1];
+    console.log(attempt);
+    res.send(attempt);
+});
+
+export default router
