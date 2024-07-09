9181eba9abc0488d0e6ba5d13d3dbde58aca187d
UBC Student <czheng17@students.cs.ubc.ca>
2024-07-09 16:25:15 -0700

    Update AttemptPage.js

--- client/src/pages/AttemptPage.js
+++ client/src/pages/AttemptPage.js
@@ -1,6 +1,8 @@
 import './AttemptPage.css';
+import { useParams } from 'react-router-dom';
 import React, {useState, useEffect} from 'react';
-import {useNavigate,useLocation} from 'react-router-dom';
+import {useNavigate} from 'react-router-dom';
+import axios from "axios";
 
 /*
     Props Required:
@@ -9,7 +11,10 @@ import {useNavigate,useLocation} from 'react-router-dom';
         - username (string): The user's username. Used to find and store attempt info
         - password (string): The user's password. Used for authentication purposes with the API calls.
  */
+
 function AttemptPage(props) {
+    const { id: question_id,attemptId:attemptId,username:username} = useParams();
+    const numericAttemptId = parseInt(attemptId);
     // Initializing states and state hooks.
     const [functionText, setFunctionText] = useState("");
 
@@ -19,8 +24,8 @@ function AttemptPage(props) {
     const [failingTestCases, setFailingTestCases] = useState("");
     const [generatedCode, setGeneratedCode] = useState("");
 
-    const question_id = props.question_id;
-    const [attemptNum, setAttemptNum] = useState(props.attempt_num);
+    // const question_id = props.question_id;
+    const [attemptNum, setAttemptNum] = useState("");
     const [testsCorrect, setTestsCorrect] = useState(0);
     const [testsTotal, setTestsTotal] = useState(0);
     const [duration, setDuration] = useState(0);
@@ -37,55 +42,48 @@ function AttemptPage(props) {
     const endpoint = "/users/" + props.username + "/questions/" + props.question_id;
     // For refreshing the page, use reloadPage()
     const navigate = useNavigate();
-    const userInfo = useLocation().state;
-
     const reloadPage = () => {
-        navigate(0);
+        // navigate(/questions);
     };
 
-    useEffect(() => {
-        if (userInfo === null) {
-          navigate("/")
-        } 
-      })
+    // get the corresponding data from the questionid and check user whether done this question before?
+    // if not, create the new attempt; or return the latest attempt?
 
-    // Set up all the variables based on saved attempt data
     useEffect(() => {
-        const fetchData = async () => {
+        const fetchQuestionData = async () => {
             try {
-                // TODO: BUG here. 500 internal server error.
-                const response = await fetch(endpoint + "/attempts/" + attemptNum, {
-                    method: 'PUT',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({password: props.password})
-                });
-                if (!response.ok) {
-                    console.log(response);
-                    // TODO: Fix here. Refresh the page if the response is not ok.
-                    //  very hacky and could potentially lead to an infinite loop.
-                    reloadPage();
+                const questionsResponse  = await axios.get(`/questions/${question_id}`);
+                const userResponse  = await axios.get(`/users/Student_A`) ;
+                const questions = questionsResponse.data;
+                const user = userResponse.data;
+                const userQuestion = user.questions.find(q => q.questionId === parseInt(question_id));
+                console.log(userQuestion);
+                if (userQuestion && userQuestion.attempts.length === 0) {
+                    setAttemptNum(1);
+                    setFunctionText(questions.code); 
+                } 
+                else {
+                    //need to change dynamcially corresponding to the questions page
+                    const attmeptNum=attemptId;
+                    setAttemptNum(attmeptNum);
+                    setFunctionText(questions.code); 
+                    setDescription(userQuestion.attempts[attmeptNum].description);
+                    setNotes(userQuestion.attempts[attmeptNum].notes);
+                    setFailingTestCases(userQuestion.attempts[attmeptNum].failingTestCases);
+               
                 }
-                const result = await response.json();
-
-                setInProgress(result.inProgress);
-                setDescription(result.description);
-                setNotes(result.notes);
-                setGeneratedCode(result.generatedCode);
-                setFailingTestCases(result.failingTestCases);
-                setTestsCorrect(result.testCorrect);
-                setTestsTotal(result.testTotal);
-                setDuration(result.duration);
-                setFunctionText(result.question);
-            } catch (err) {
-                // For debugging
-                console.log(err.message);
-                // setFunctionText(err.message);
+             
+               
+            } catch (error) {
+                console.error('Error fetching question:', error);
             }
-        }
-        fetchData();
-    }, [endpoint, attemptNum, props.password]);
+        };
+    
+        fetchQuestionData(); 
+    
+    }, [question_id,username]); 
+
+
 
 
     const handleDescription = (event) => {
@@ -96,7 +94,7 @@ function AttemptPage(props) {
         setNotes(event.target.value);
     }
 
-    const handleSubmit = async () => {
+    const submit = async () => {
         setSubmitEnabled(false);
         const input = {
             password: props.password,
@@ -104,7 +102,6 @@ function AttemptPage(props) {
             notes: notes,
             inProgress: false
         }
-
         try {
             const response = await fetch(endpoint, {
                 method: "PUT", headers: {
@@ -122,33 +119,56 @@ function AttemptPage(props) {
         } finally {
             setSubmitEnabled(true);
         }
+    }
+
+    const handleSubmit = () => {
+        submit();
     };
 
-    const handleSave = async () => {
+    // const save = async () => {
+    //     setSaveEnabled(false);
+        
+    //     const input = {
+    //         password: props.password, description: description, notes: notes, inProgress: true
+    //     }
+    //     try {
+    //         const response = await fetch(endpoint, {
+    //             method: "PUT", headers: {
+    //                 'Content-Type': 'application/json'
+    //             }, body: JSON.stringify(input)
+    //         });
+
+    //         if (!response.ok) {
+    //             console.log("Could not fetch data. Code " + response.status);
+    //         } else {
+    //             reloadPage();
+    //         }
+    //     } catch (err) {
+    //         console.log("There was a problem saving the attempt: " + err);
+    //     } finally {
+    //         setSaveEnabled(true);
+    //     }
+    // }
+    const save = async () => {
         setSaveEnabled(false);
         const input = {
-            password: props.password,
             description: description,
             notes: notes,
             inProgress: true
-        }
+        };
         try {
-            const response = await fetch(endpoint, {
-                method: "PUT", headers: {
-                    'Content-Type': 'application/json'
-                }, body: JSON.stringify(input)
-            });
-
-            if (!response.ok) {
-                console.log("Could not fetch data. Code " + response.status);
-            } else {
-                reloadPage();
-            }
-        } catch (err) {
-            console.log("There was a problem saving the attempt: " + err);
+            const response = await axios.put(`/users/${username}/questions/${question_id}`, input);
+            console.log('Data saved successfully:', response.data);
+            reloadPage();
+        } catch (error) {
+            console.error('There was a problem saving the attempt:', error);
         } finally {
             setSaveEnabled(true);
         }
+    };
+
+    const handleSave = () => {
+        save();
     }
 
     const retry = () => {
@@ -201,15 +221,7 @@ function AttemptPage(props) {
 
     // For return button
     const handleReturn = () => {
-        navigate("/questions", {state: userInfo});
-    }
-
-    const onHomeButtonClicked = () => {
-        navigate("/home", {state: userInfo});
-    }
-    
-    const onProfileButtonClicked = () => {
-        navigate("/profile", {state: userInfo});
+        navigate("/questions");
     }
 
 
@@ -224,80 +236,67 @@ function AttemptPage(props) {
         scoreColour = "tomato";
     }
 
-    return (<div className="AttemptPage">
-        {/* <h1 style={{color: "black",}}>Question: {question_id} - Attempt: #{attemptNum}</h1> */}
-        <div className="header">
-            <button title="Go To Home Page" className='homeButton' onClick={onHomeButtonClicked}><span className='headerSpan'>Home</span></button>
-            <button className="returnButton" title="Back"  onClick={handleReturn}><span className='headerSpan'>Return</span></button>
-            <h1 className='headerTitleAttempt'>Question: {question_id} - Attempt: #{attemptNum}</h1>
-            <button title="Go To Profile Page" className='profileButton' onClick={onProfileButtonClicked}><span className='headerSpan'>Profile</span></button>
+    return (
+        <div className="AttemptPage">
+            <h1 style={{ color: "black" }}>Question: {question_id} - Attempt # {numericAttemptId+1}</h1>
+            <button className="return-button" onClick={handleReturn}>Return</button>
+            {!isInProgress ? <div>
+                <h2 style={{ color: scoreColour }}>{testsCorrect}/{testsTotal}&emsp;&emsp;{duration}s</h2>
+            </div> : <h2 style={{ color: "darkorchid" }}>Attempt In Progress</h2>}
+
+            <h2>Formulate the functionality of the following foo function</h2>
+
+            <div className="grid-container">
+                <pre className="grid-item function-text">
+                    {functionText}
+                </pre>
+
+                {!isInProgress ? <pre className="grid-item function-text" style={{ color: 'mediumslateblue' }}>
+                    {generatedCode}
+                </pre> : <pre className="grid-item" style={{ textAlign: 'left', backgroundColor: "#f4f4f4" }}>
+                    Submit to see your LLM generated code!
+                </pre>}
+
+                <textarea
+                    className={`grid-item ${isInProgress ? "" : "readonly-textarea"}`}
+                    style={{ fontFamily: 'Helvetica', textAlign: 'left' }}
+                    placeholder="Input your description here..."
+                    onChange={handleDescription}
+                    value={description}
+                    readOnly={!isInProgress}
+                />
+
+                {isInProgress ? <pre className="grid-item" id="failing-test-case-box" style={{ fontFamily: 'Arial, sans-serif', textAlign: 'left' }}>
+                    Submit to see if you have any failing test cases
+                </pre> : <pre className="grid-item" id="failing-test-case-box" style={{ fontFamily: 'Arial, sans-serif', textAlign: 'left', color: 'red' }}>
+                    {failingTestCases}
+                </pre>}
+
+                <textarea
+                    className={`grid-item ${isInProgress ? "" : "readonly-textarea"}`}
+                    style={{ textAlign: 'left' }}
+                    placeholder="Write notes here (optional)"
+                    onChange={handleNotes}
+                    value={notes}
+                    readOnly={!isInProgress}
+                />
+            </div>
+
+            <h2> </h2> {/* Just for some separation between the question side and the save/submitting stuff*/}
+
+            {isInProgress ? <div>
+                {saveEnabled ? <button className="active-button" onClick={handleSave}>Save</button> :
+                    <button className="inactive-button" disabled>Saving...</button>}
+                {submitEnabled ? <button className="active-button" onClick={handleSubmit}>Submit</button> :
+                    <button className="inactive-button" disabled>Submitting...</button>}
+            </div> : <div>
+                {retryEnabled ? <button className="active-button" onClick={handleRetry}>Retry</button> :
+                    <button className="inactive-button" disabled>Initializing new attempt...</button>}
+            </div>}
+
+            {isInProgress && <h5>Please allow some time for submission, generating code can take a while.</h5>}
         </div>
-        {!isInProgress ? <div className='timer'>
-            <h2 style={{color: scoreColour}}>{testsCorrect}/{testsTotal}&emsp;&emsp;{duration}s</h2>
-        </div> : <h2 style={{color: "darkorchid"}}>Attempt In Progress</h2>}
-
-        <h2>Formulate the functionality of the following foo function</h2>
-
-        <div className="grid-container">
-        <pre className="grid-item function-text">
-            {functionText}
-        </pre>
-
-            {!isInProgress ? <pre className="grid-item function-text" style={{color: 'mediumslateblue'}}>
-              {generatedCode}
-          </pre> : <pre className="grid-item" style={{textAlign: 'left', backgroundColor: "#f4f4f4"}}>
-            Submit to see your LLM generated code!
-          </pre>}
-
-            {isInProgress ? <textarea
-                className="grid-item"
-                style={{fontFamily: 'Helvetica', textAlign: 'left'}}
-                placeholder="Input your description here..."
-                onChange={handleDescription}
-            /> : <textarea
-                onChange={handleDescription}
-                className="grid-item readonly-textarea"
-                style={{fontFamily: 'Helvetica', textAlign: 'left'}}
-                value={description}
-                readOnly
-            />}
-
-            {isInProgress ? <pre className="grid-item" id="failing-test-case-box"
-                                 style={{fontFamily: 'Arial, sans-serif', textAlign: 'left'}}>
-            Submit to see if you have any failing test cases
-            </pre> : <pre className="grid-item" id="failing-test-case-box"
-                          style={{fontFamily: 'Arial, sans-serif', textAlign: 'left', color: 'red'}}>
-            {failingTestCases}
-            </pre>}
-
-            {isInProgress ? <textarea
-                className="grid-item"
-                style={{textAlign: 'left'}}
-                placeholder="Write notes here (optional)"
-                onChange={handleNotes}
-            /> : <textarea onChange={handleNotes}
-                           className="grid-item readonly-textarea"
-                           style={{textAlign: 'left'}}
-                           value={notes}
-                           readOnly
-            />}
-        </div>
-
-        <h2> </h2> {/* Just for some separation between the question side and the save/submitting stuff*/}
-
-        {isInProgress ? <div>
-            {saveEnabled ? <button className="active-button" onClick={handleSave}>Save</button> :
-                <button className="inactive-button" disabled>Saving...</button>}
-            {submitEnabled ? <button className="active-button" onClick={handleSubmit}>Submit</button> :
-                <button className="inactive-button" disabled>Submitting...</button>}
-        </div> : <div>
-            {retryEnabled ? <button className="active-button" onClick={handleRetry}>Retry</button> :
-                <button className="inactive-button" disabled>Initializing new attempt...</button>}
-        </div>}
-
-        {isInProgress && <h5>Please allow some time for submission, generating code can take a while.</h5>}
-
-    </div>);
+    );
 }
 
 export default AttemptPage;
