16ebc38ddf1525cd14b5ecda5af11cb4f33778fb
mkoprivi <mkoprivi@student.ubc.ca>
2024-07-17 12:32:49 -0700

    Save/Submit and View Attempt tests

--- test/users.test.js
+++ test/users.test.js
@@ -439,204 +439,123 @@ describe('Save and Submit Attempt', () => {
 	const usersJsonPath = '../server/data/users.json';
 	const usersJSON = readJsonFile(usersJsonPath);
 	const questionsJsonPath = '../server/data/questions.json';
-	const questoinsJSON = readJsonFile(questionsJsonPath);
+	const questionsJSON = readJsonFile(questionsJsonPath);
 
 	afterEach(function() {
         writeJsonFile(usersJsonPath, usersJSON);
-		writeJsonFile(questionsJsonPath, questoinsJSON);
-    });
-	
-  /*
-  saving this for now (lots of changes in main make it difficult to merge
-  
-  describe("View Questions API", () => {
-        it('View Questions Success', async () => {
-            const username = "Student_A";
-            const password = "pStudent_A";
-            const expected_ret = {
-                description: "This function always returns 1",
-                notes: "Just testing partial success... I know what the function does...",
-                inProgress: false,
-                startTime: "2024-07-14T18:05:57.721Z",
-                endTime: "2024-07-14T18:17:38.296Z",
-                duration: 700.575,
-                generatedCode: "function foo(x) {\n return \"An error occurred when generating the code\";\n}",
-                failingTestCases: "foo([]) should return 0\nfoo([1]) should return 1\nfoo([1, 2]) should return 3\nfoo([-2, 2]) should return 0\nfoo([5, 13, -4]) should return 14\nfoo([9, 10, 21]) should return 40\n",
-                testCorrect: 0,
-                testTotal: 6,
-                question: "function foo(n) {\n  var val = 0;\n  for (i = 0; i < n.length; i++) {\n val += n[i];\n  }\n return val;\n}"
-            }
-            // Student_A log-in
-            await axios.put("http://localhost:5000/users/login", {
-                username,
-                password
-            });
-            // Views questions
-            const response = await axios.put(
-                "http://localhost:5000/users/Student_A/questions/2/attempts/2",
-                { password: password });
-            const data = response.data;
-            expect(response.status).to.eql(200);
-            expect(data).to.eql(expected_ret);
-        });
-
-        it('View Questions Failure - User not logged in', async () => {
-            const password = "pStudent_A";
-            const expected_ret = "";
-
-            try {
-                const response = await axios.put(
-                    "http://localhost:5000/users/Student_A/questions/2/attempts/2",
-                    { password: password });
-                const data = response.data;
-            } catch (err) {
-                expect(err.response.status).to.eql(401);
-                expect(err.response.data.error).to.eql("User is not currently logged in");
-            }
-        });
-
-        it('View Questions Failure - Wrong Password', async () => {
-            const password = "bad_password";
-            const expected_ret = "";
-
-            try {
-                const response = await axios.put(
-                    "http://localhost:5000/users/Student_A/questions/2/attempts/2",
-                    { password: password });
-            } catch (err) {
-                expect(err.response.status).to.eql(401);
-            }
-        });
+		writeJsonFile(questionsJsonPath, questionsJSON);
     });
 
+	it('Save Attempt Success', async () => {
+		const username = "Student_A";
+		const password = "pStudent_A";
+		const expected_ret = {
+			description: "This function always returns 1",
+			notes: "Just testing partial success... I know what the function does...",
+			inProgress: true,
+			startTime: "2024-07-01T07:00:00.000Z",
+			endTime: null,
+			duration: null,
+			generatedCode: null,
+			failingTestCases: null,
+			testCorrect: null,
+			testTotal: null,
+			question: "function foo() {\n return \"Hello World!\";\n}"
+		}
+		// Student_A log-in
+		await axios.put("http://localhost:5000/users/login", {
+			username,
+			password
+		});
+		// Save question
+		const response = await axios.put(
+			"http://localhost:5000/users/Student_A/questions/1",
+			{
+				password: "pStudent_A",
+				description: "This function always returns 1",
+				notes: "Just testing partial success... I know what the function does...",
+				inProgress: true
+			});
+		expect(response.status).to.eql(204);
+		// View if saved
+		const view_response = await axios.put(
+			"http://localhost:5000/users/Student_A/questions/1/attempts/4",
+			{ password: password });
+		const view_data = view_response.data;
+		expect(view_response.status).to.eql(200);
+		expect(view_data).to.eql(expected_ret);
+	});
 
-    describe("Save/Submit Attempt API", () => {
-        it('Save Attempt Success', async () => {
-            const username = "Student_A";
-            const password = "pStudent_A";
-            const expected_ret = {
-                description: "This function always returns 1",
-                notes: "Just testing partial success... I know what the function does...",
-                inProgress: true,
-                startTime: "2024-07-01T07:00:00.000Z",
-                endTime: null,
-                duration: null,
-                generatedCode: null,
-                failingTestCases: null,
-                testCorrect: null,
-                testTotal: null,
-                question: "function foo(n) {\n  var val = 0;\n  for (i = 0; i < n.length; i++) {\n val += n[i];\n  }\n return val;\n}"
-            }
-            // Student_A log-in
-            await axios.put("http://localhost:5000/users/login", {
-                username,
-                password
-            });
-            // Save question
-            const response = await axios.put(
-                "http://localhost:5000/users/Student_A/questions/2",
-                {
-                    password: "pStudent_A",
-                    description: "This function always returns 1",
-                    notes: "Just testing partial success... I know what the function does...",
-                    inProgress: true
-                });
-            expect(response.status).to.eql(204);
-            // View if saved
-            const view_response = await axios.put(
-                "http://localhost:5000/users/Student_A/questions/2/attempts/3",
-                { password: password });
-            const view_data = view_response.data;
-            expect(view_response.status).to.eql(200);
-            expect(view_data).to.eql(expected_ret);
-        });
+	it('Submit Attempt Success', async () => {
+		const username = "Student_A";
+		const password = "pStudent_A";
 
-        it('Submit Attempt Success', async () => {
-            const username = "Student_A";
-            const password = "pStudent_A";
-            const expected_ret = {
-                description: "This function always returns 1",
-                notes: "Just testing partial success... I know what the function does...",
-                inProgress: true,
-                startTime: "2024-07-01T07:00:00.000Z",
-                endTime: null,
-                duration: null,
-                generatedCode: null,
-                failingTestCases: null,
-                testCorrect: null,
-                testTotal: null,
-                question: "function foo(n) {\n  var val = 0;\n  for (i = 0; i < n.length; i++) {\n val += n[i];\n  }\n return val;\n}"
-            }
-            // Student_A log-in
-            await axios.put("http://localhost:5000/users/login", {
-                username,
-                password
-            });
+		// Student_A log-in
+		await axios.put("http://localhost:5000/users/login", {
+			username,
+			password
+		});
 
-            // Submit question
-            const response = await axios.put(
-                "http://localhost:5000/users/Student_A/questions/2",
-                {
-                    password: "pStudent_A",
-                    description: "This function always returns 1",
-                    notes: "Just testing partial success... I know what the function does...",
-                    inProgress: false
-                });
+		// Submit question
+		const response = await axios.put(
+			"http://localhost:5000/users/Student_A/questions/1",
+			{
+				password: "pStudent_A",
+				description: "This function always returns 1",
+				notes: "Just testing partial success... I know what the function does...",
+				inProgress: false
+			});
 
-            expect(response.status).to.eql(204);
+		expect(response.status).to.eql(204);
 
-            // View if saved
-            const view_response = await axios.put(
-                "http://localhost:5000/users/Student_A/questions/2/attempts/3",
-                { password: password });
-            const view_data = view_response.data;
-            expect(view_response.status).to.eql(200);
+		// View if saved
+		const view_response = await axios.put(
+			"http://localhost:5000/users/Student_A/questions/1/attempts/4",
+			{ password: password });
+		const view_data = view_response.data;
+		expect(view_response.status).to.eql(200);
 
-            // Stuff changes per submit, so just test if it is not in the starting state
-            expect(view_data.generatedCode).to.not.eql(null);
-            expect(view_data.duration).to.not.eql(null);
-            expect(view_data.endTime).to.not.eql(null);
-            expect(view_data.failingTestCases).to.not.eql(null);
-        });
+		// Stuff changes per submit, so just test if it is not in the starting state
+		expect(view_data.inProgress).to.eql(false);
+		expect(view_data.generatedCode).to.not.eql(null);
+		expect(view_data.duration).to.not.eql(null);
+		expect(view_data.endTime).to.not.eql(null);
+		expect(view_data.failingTestCases).to.not.eql(null);
+		
+	}).timeout(100000); //allow time to generate code
 
-        it('Save/Submit Questions Failure - User not logged in', async () => {
-            const password = "pStudent_A";
+	it('Save/Submit Questions Failure - User not logged in', async () => {
+		const password = "pStudent_A";
 
-            try {
-                const response = await axios.put(
-                    "http://localhost:5000/users/Student_A/questions/2",
-                    {
-                        password: "pStudent_A",
-                        description: "This function always returns 1",
-                        notes: "Just testing partial success... I know what the function does...",
-                        inProgress: true
-                    });
-            } catch (err) {
-                expect(err.response.status).to.eql(401);
-                expect(err.response.data.error).to.eql("User is not currently logged in");
-            }
-        });
+		try {
+			const response = await axios.put(
+				"http://localhost:5000/users/Student_A/questions/2",
+				{
+					password: "pStudent_A",
+					description: "This function always returns 1",
+					notes: "Just testing partial success... I know what the function does...",
+					inProgress: true
+				});
+		} catch (err) {
+			expect(err.response.status).to.eql(401);
+			expect(err.response.data.error).to.eql("User is not currently logged in");
+		}
+	});
 
-        it('Save/Submit Questions Failure - Wrong Password', async () => {
-            try {
-                const response = await axios.put(
-                    "http://localhost:5000/users/Student_A/questions/2",
-                    {
-                        password: "bad_password",
-                        description: "This function always returns 1",
-                        notes: "Just testing partial success... I know what the function does...",
-                        inProgress: true
-                    });
-            } catch (err) {
-                expect(err.response.status).to.eql(401);
-            }
-        });
-    });
-
-    */
-	
-	
-	
+	it('Save/Submit Questions Failure - Wrong Password', async () => {
+		try {
+			const response = await axios.put(
+				"http://localhost:5000/users/Student_A/questions/2",
+				{
+					password: "bad_password",
+					description: "This function always returns 1",
+					notes: "Just testing partial success... I know what the function does...",
+					inProgress: true
+				});
+		} catch (err) {
+			expect(err.response.status).to.eql(401);
+		}
+	});
 });
 
 describe('View Attempt', () => {
@@ -650,6 +569,64 @@ describe('View Attempt', () => {
         writeJsonFile(usersJsonPath, usersJSON);
 		writeJsonFile(questionsJsonPath, questoinsJSON);
     });
+
+	it('View Attempt Success', async () => {
+		const username = "Student_A";
+		const password = "pStudent_A";
+		const expected_ret = {
+			description: "description",
+			notes: "note",
+			inProgress: false,
+			startTime: "2024-07-01T07:00:00.000Z",
+			endTime: "2024-07-01T07:10:00.000Z",
+			duration: 600,
+			generatedCode: "LLM",
+			failingTestCases: "testsFailed",
+			testCorrect: 1,
+			testTotal: 2,
+			question: "function foo(n) {\n  var val = 0;\n  for (i = 0; i < n.length; i++) {\n val += n[i];\n  }\n return val;\n}"
+		}
+		// Student_A log-in
+		await axios.put("http://localhost:5000/users/login", {
+			username,
+			password
+		});
+		// Views questions
+		const response = await axios.put(
+			"http://localhost:5000/users/Student_A/questions/2/attempts/1",
+			{ password: password });
+		const data = response.data;
+		expect(response.status).to.eql(200);
+		expect(data).to.eql(expected_ret);
+	});
+
+	it('View Attempt Failure - User not logged in', async () => {
+		const password = "pStudent_A";
+		const expected_ret = "";
+
+		try {
+			const response = await axios.put(
+				"http://localhost:5000/users/Student_A/questions/2/attempts/1",
+				{ password: password });
+			const data = response.data;
+		} catch (err) {
+			expect(err.response.status).to.eql(401);
+			expect(err.response.data.error).to.eql("User is not currently logged in");
+		}
+	});
+
+	it('View Attempt Failure - Wrong Password', async () => {
+		const password = "bad_password";
+		const expected_ret = "";
+
+		try {
+			const response = await axios.put(
+				"http://localhost:5000/users/Student_A/questions/2/attempts/1",
+				{ password: password });
+		} catch (err) {
+			expect(err.response.status).to.eql(401);
+		}
+	});
 	
 });
 
@@ -678,10 +655,10 @@ describe('View Questions', () => {
 				});
 				expect(res.status).to.equal(200);
 			} catch (err) {
-					expect(err.response.status).equal(404);		
+				expect(err.response.status).equal(404);
 			}
 		});
-	});	
+	});
 });
 
 describe('Delete Question', () => {
@@ -691,14 +668,14 @@ describe('Delete Question', () => {
 	const questionsJsonPath = '../server/data/questions.json';
 	const questoinsJSON = readJsonFile(questionsJsonPath);
 
-	afterEach(function() {
-        writeJsonFile(usersJsonPath, usersJSON);
+	afterEach(function () {
+		writeJsonFile(usersJsonPath, usersJSON);
 		writeJsonFile(questionsJsonPath, questoinsJSON);
-    });
-	
-	
-	
-	
+	});
+
+
+
+
 });
 
 describe('Add Question', () => {
